2bce1afc0fba586573688115b63fde4099262078
Ingrid Silva <ingrid_flack@hotmail.com>
2022-10-16 16:35:18 -0300

    Add Keyboard component html structure

---
+++ src/components/Keyboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const KeyboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-itens: center;
+  justify-content: center;
+`;
+
+export const Row = styled.div`
+  margin-top: 10px;
+  display: flex;
+  align-itens: center;
+  justify-content: center;
+`;
